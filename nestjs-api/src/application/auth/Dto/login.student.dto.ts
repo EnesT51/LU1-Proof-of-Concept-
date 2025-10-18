0aaa49507c003e5e0c90dd8751cf489c84345327
EnesT51 <Enes.t51@outlook.com>
2025-10-19 01:17:02 +0200

    validation voor alle input af

--- nestjs-api/src/application/auth/Dto/login.student.dto.ts
+++ nestjs-api/src/application/auth/Dto/login.student.dto.ts
@@ -2,10 +2,9 @@ import { IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
 
 export class LoginStudentDto {
     @IsEmail({},{ message: 'Ongeldig e-mailadres' })
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Email is verplicht' })
     email: string;
 
     @IsNotEmpty({ message: 'Wachtwoord is verplicht' })
-    @Length(6, 30)
     password: string;
 }
