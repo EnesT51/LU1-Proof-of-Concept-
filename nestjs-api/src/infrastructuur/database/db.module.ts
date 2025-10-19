5132704fbdbf7c965d32ade8e56dafe9851d8ee2
EnesT51 <Enes.t51@outlook.com>
2025-10-19 19:27:09 +0200

    commit

--- nestjs-api/src/infrastructuur/database/db.module.ts
+++ nestjs-api/src/infrastructuur/database/db.module.ts
@@ -4,10 +4,9 @@ import { Module } from '@nestjs/common';
 dotenv.config();
 // Replace the uri string with your MongoDB deployment's connection string.
 
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db.jwngav9.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db.jwngav9.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
-const uri = process.env.CONNECTION_STRING as string;
 @Module({
     providers: [
         {
